0524ef540865e0c5beb56e292b1f5e5c034998ee
Super <SuperL2080@gmail.com>
2018-01-08 03:55:38 +0800

    finish channel api

--- server/main.js
+++ server/main.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const middleware = require('./middleware');
 const wechatOpenRouter = require('./routes/wechatOpen');
 const wechatMpRouter = require('./routes/wechatMp');
-const deviceRouter = require('./routes/device');
+const serviceRouter = require('./routes/service');
 
 
 mongoose.connect(process.env.MONGO_URL, {useMongoClient: true});
@@ -13,10 +13,11 @@ const router = exports.router = express.Router();
 
 router.use('/', middleware.router);
 
+router.post('/device/update', serviceRouter.deviceUpdate);
+router.post('/channel/subscribe', serviceRouter.channelSubscribe);
+
 router.post('/wechat/open/authNotice', wechatOpenRouter.authNotice);
 router.post('/wechat/open/adNotice/:appid', wechatOpenRouter.adNotice);
-
-router.post('/device/update', deviceRouter.update);
 
 router.get('/wechat/open/adAuth', wechatOpenRouter.adAuth);
 router.get('/wechat/open/adAuthSuccess', wechatOpenRouter.adAuthSuccess);
---
+++ server/routes/service.js
@@ -0,0 +1,68 @@
+
+const async = require('async');
+const pointModel = require('../../imports/models/point');
+const partnerModel = require('../../imports/models/partner');
+
+
+const deviceUpdate = exports.deviceUpdate = (req, res, next) => {
+    console.log('[CALL] deviceUpdate, body:');
+    console.log(req.body);
+
+    async.auto({
+        GetDefaultPartner: (callback) => {
+            console.log('[CALL] deviceUpdate, GetDefaultPartner');
+            partnerModel.GetDefaultPartner(null, callback);
+        },
+
+        UpdatePoint: ['GetDefaultPartner', (result, callback) => {
+            console.log('[CALL] deviceUpdate, UpdatePoint');
+            pointModel.UpdateZhijinji({
+                partnerId: result.GetDefaultPartner._id,
+                devNo: req.body.devNo,
+                type: req.body.type,
+                state: req.body.state
+            }, callback);
+        }]
+
+    }, (err, result) => {
+        console.log('[CALLBACK] deviceUpdate');
+        let json = {
+            code: 0,
+            data: {
+                res: 'SUCCESS'
+            },
+            message: 'Update device info success.'
+        };
+        if( err ) {
+            json.code = 20001;
+            json.data.res = 'FAIL';
+            json.message = err.message;
+        }
+        res.send(json);
+    });
+}
+
+const channelSubscribe = exports.channelSubscribe = (req, res, next) => {
+    console.log('[CALL] channelSubscribe, body:');
+    console.log(req.body);
+
+    orderHelper.AdSubscribe({
+        userId: req.body.userId,
+        appid: req.body.appid
+    }, (err, result) => {
+        console.log('[CALLBACK] channelSubscribe');
+        let json = {
+            code: 0,
+            data: {
+                res: 'SUCCESS'
+            },
+            message: 'Channel subscribe success.'
+        };
+        if( err ) {
+            json.code = 20002;
+            json.data.res = 'FAIL';
+            json.message = err.message;
+        }
+        res.send(json);
+    });
+}
