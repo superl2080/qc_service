5db1cfcaf50f9e9f8062425f425e3e32a4253ed4
Super <SuperL2080@gmail.com>
2018-01-30 11:40:20 +0800

    for v1.4

--- app/main.js
+++ app/main.js
@@ -3,39 +3,6 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 
 
-const loadModules = async (path, objs) => {
-    const files = fs.readdirSync(path);
-    for( let file of files ){
-        const pos = file.lastIndexOf('.');
-        if( pos < 0 )
-            continue;
-        const filePrefix = file.substr(0, pos);
-        const filePostfix = file.substr(pos + 1);
-        if( filePrefix.length < 1 || filePostfix.length < 1 || filePostfix != 'js' )
-            continue;
-        const module = require(path + '/' + filePrefix);
-        for( let obj of objs ){
-            if( obj[filePrefix] === undefined ){
-                obj[filePrefix] = module;
-            } else {
-                for( let i = 1; i >= 0; i++ ){
-                    if( obj[filePrefix + i] === undefined ){
-                        obj[filePrefix + i] = module;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-};
-
-const setModules = async (obj, name, module) => {
-    for( let key of Object.keys(obj) ){
-        obj[key][name] = module;
-    }
-};
-
-
 module.exports = {
 
     modules: { },
@@ -53,13 +20,15 @@ module.exports = {
     run: async app => {
         mongoose.connect(process.env.MONGO_URL, { useMongoClient: true });
 
-        loadModules('./controllers', [this.controllers, this.modules]);
-        loadModules('./models', [this.models, this.modules]);
-        loadModules('./models/apis', [this.models.apis, this.modules]);
-        loadModules('./models/dbs', [this.models.dbs, this.modules]);
-        loadModules('./models/utils', [this.models.utils, this.modules]);
-        loadModules('./tests', [this.tests, this.modules]);
-        setModules(this.modules, 'models', this.models);
+        await this.loadModules(path.join(__dirname, 'controllers'), [this.controllers, this.modules]);
+        await this.loadModules(path.join(__dirname, 'models'), [this.models, this.modules]);
+        await this.loadModules(path.join(__dirname, 'models/apis'), [this.models.apis, this.modules]);
+        await this.loadModules(path.join(__dirname, 'models/dbs'), [this.models.dbs, this.modules]);
+        await this.loadModules(path.join(__dirname, 'models/utils'), [this.models.utils, this.modules]);
+        await this.loadModules(path.join(__dirname, 'tests'), [this.tests, this.modules]);
+        await this.setModules(this.modules, 'models', this.models);
+
+        console.log(this.modules);
 
         if( process.env.NODE_ENV == 'test' ) {
             this.tests.main.run(app);
@@ -86,6 +55,38 @@ module.exports = {
         app.get('/wechat/open/adAuthCbk',           this.controllers.wechatOpen.adAuthCbk);
 
         app.post('/wechat/pay/notice',              this.controllers.wechatPay.notice);
+    },
+
+    loadModules: async (path, objs) => {
+        const files = fs.readdirSync(path);
+        for( let file of files ){
+            const pos = file.lastIndexOf('.');
+            if( pos < 0 )
+                continue;
+            const filePrefix = file.substr(0, pos);
+            const filePostfix = file.substr(pos + 1);
+            if( filePrefix.length < 1 || filePostfix.length < 1 || filePostfix != 'js' )
+                continue;
+            const module = require(path + '/' + filePrefix);
+            for( let obj of objs ){
+                if( obj[filePrefix] === undefined ){
+                    obj[filePrefix] = module;
+                } else {
+                    for( let i = 1; i >= 0; i++ ){
+                        if( obj[filePrefix + i] === undefined ){
+                            obj[filePrefix + i] = module;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    },
+
+    setModules: async (obj, name, module) => {
+        for( let key of Object.keys(obj) ){
+            obj[key][name] = module;
+        }
     },
 
 };
