b59ff195919ad57ff75ace97253a45446677a47b
Super <SuperL2080@gmail.com>
2018-02-01 14:00:30 +0800

    for v1.4

--- app/controllers/jobs.js
+++ app/controllers/jobs.js
@@ -3,12 +3,12 @@
 module.exports = {
 
     run: async function () {
-        setInterval(this.cleaningOrder.bind(this), 1 * 60 * 1000);
+        setInterval(this.cleaningOrder.bind(this), 2 * 60 * 1000);
     },
 
     cleaningOrder: async function () {
         console.log(__filename + '\n[CALL] cleaningOrder');
-        const expiresInDate = await this.models.utils.time.createExpiresInDate({ expires_in: -10 * 60 });
+        const expiresInDate = await this.models.utils.time.createExpiresInDate({ expires_in: -15 * 60 });
         await this.models.dbs.order.cancel({ expiresInDate: expiresInDate });
     },
 
--- app/models/utils/time.js
+++ app/models/utils/time.js
@@ -3,26 +3,18 @@
 module.exports = {
 
     createExpiresInDate: async function (param) {
-        console.log(__filename + '\n[CALL] createExpiresInDate, param:');
-        console.log(param);
 
         const expiresInDate = new Date();
         expiresInDate.setTime(expiresInDate.getTime() + param.expires_in * 1000 - 1 * 60 * 1000);
 
-        console.log('[CALLBACK] createExpiresInDate, result:');
-        console.log(expiresInDate);
         return expiresInDate;
     },
 
     checkExpiresInDate: async function (param) {
-        console.log(__filename + '\n[CALL] checkExpiresInDate, param:');
-        console.log(param);
 
         const currentDate = new Date();
         const result = currentDate.getTime() < param.expiresInDate.getTime();
 
-        console.log('[CALLBACK] checkExpiresInDate, result:');
-        console.log(result);
         return result;
     },
 
